6707ade871f7bea6f006d6839a2eb3ece551f25c
Praveen Singh <praveen969500@gmail.com>
2025-01-24 21:07:15 +0530

    implemented send otp verification email.

--- models/Otp.js
+++ models/Otp.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
 const otpSchema = new mongoose.Schema({
     email: {
@@ -15,5 +16,22 @@ const otpSchema = new mongoose.Schema({
         expires: 5 * 60, // expire in 5 minutes
     }
 });
+
+
+//fxn to send emails for otp
+async function sendVerificationEmail(email, otp) {
+    try {
+        const mailResponse = await mailSender(email, "Verification Email from edTech | Praveen ", otp);
+        console.log("Email sent successfully:", mailResponse);
+    } catch (error) {
+        console.log("error occured in otp.js file:", error);
+        throw error;
+    }
+}
+
+otpSchema.pre("save", async function (next) {
+    await sendVerificationEmail(this.email, this.otp);
+    next();
+})
 
 module.exports = mongoose.model('OTP', otpSchema);
